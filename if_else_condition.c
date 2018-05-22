12d23eca43b9b5b0b8d8511dbd14af3d8dae18c9
airchang <33506678+airchang@users.noreply.github.com>
2018-05-21 19:04:06 -0700

    Create if_else_condition.c

---
+++ if_else_condition.c
@@ -0,0 +1,13 @@
+    //condition
+    int age;
+    printf("Please put your age;)");
+    scanf("%d", &age);
+    if (age > 18){
+        printf("Your age > 18 \n");
+            }
+    else if (age == 18){
+        printf("Your age  = 18 \n");
+    }
+    else{
+        printf("Your age <18 \n");
+    }
