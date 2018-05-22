98d4a86e477c023cada33c6b0476e032236302f4
airchang <33506678+airchang@users.noreply.github.com>
2018-05-21 19:02:37 -0700

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,16 @@
+    int marks = 90;
+    
+    switch(marks){
+    case 90;
+        printf("Great");
+        break;
+    case 80:
+        printf("Excellent");
+        break;
+    case 70:
+        printf("Good");
+        break;
+    default;
+        printf("Ok");
+    }
+    return 0;
