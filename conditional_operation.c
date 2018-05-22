f6a9343261ea12db9b1859694671786ef2f343b4
airchang <33506678+airchang@users.noreply.github.com>
2018-05-21 19:03:21 -0700

    Create conditional_operation.c

---
+++ conditional_operation.c
@@ -0,0 +1,6 @@
+    //conditional operation
+    int a1 = 18, b1 = 20;
+    int c1;
+    //(logical expression) ? (if non-zero (True)) : (if 0 (false))
+    c1 = (a1>b1) ? b1:a1;
+    printf("ans = %d", c1);
